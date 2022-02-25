44428a5a4cd3d02150d74883457101d43af61f69
matt-hogan <mrhogan103@gmail.com>
2022-02-24 20:11:09 -0600

    Edit parameter handling

--- index.php
+++ index.php
@@ -12,19 +12,6 @@
     $firstName = filter_input(INPUT_GET, "firstname", FILTER_SANITIZE_STRING);
     $lastName = filter_input(INPUT_GET, "lastname", FILTER_SANITIZE_STRING);
     $age = filter_input(INPUT_GET, "age", FILTER_SANITIZE_STRING);
-
-    if ($firstName == NULL || $lastName == NULL || $age == NULL) {
-        if ($firstName == NULL) {
-            echo "First name parameter is not set.<br>";
-        }
-        if ($lastName == NULL) {
-            echo "Last name parameter is not set.<br>";
-        }
-        if ($age == NULL) {
-            echo "Age parameter is not set.<br>";
-        }
-        exit;
-    }
 ?>
 
 <!DOCTYPE html>
@@ -39,26 +26,36 @@
 <body>
     <header>
         <h1>
-            <?php echo "Hello, my name is $firstName $lastName." ?>
+            <?php if ($firstName == NULL || $lastName == NULL) { 
+                echo "Hello, name parameters are not set.";
+            } else {
+                echo "Hello, my name is $firstName $lastName.";
+            } ?>
         </h1>
     </header>
     <main>
-        <h2>
-            <?php 
-                if ($age >= 18) {
-                    echo "I am $age years old and I am old enough to vote in the United States.";
-                } else {
-                    echo "I am $age years old and I am not old enough to vote in the United States.";
-                }
-            ?>
-        </h2>
-        <h3>
-            <?php 
-                $minDays = $age * 365;
-                $maxDays = ($age + 1) * 365 - 1;
-                echo "I have been alive for $minDays to $maxDays days depending on when my birthday is.";
-            ?>
-        </h3>
+        <?php if ($age == NULL) { ?>
+            <h2>
+                <?php echo "Age parameter is not set."; ?>
+            </h2>
+        <?php } else { ?>
+            <h2>
+                <?php 
+                    if ($age >= 18) {
+                        echo "I am $age years old and I am old enough to vote in the United States.";
+                    } else {
+                        echo "I am $age years old and I am not old enough to vote in the United States.";
+                    }
+                ?>
+            </h2>
+            <h3>
+                <?php 
+                    $minDays = $age * 365;
+                    $maxDays = ($age + 1) * 365 - 1;
+                    echo "I have been alive for $minDays to $maxDays days depending on when my birthday is.";
+                ?>
+            </h3>
+        <?php } ?>
     </main>
     <footer>
         <p>
